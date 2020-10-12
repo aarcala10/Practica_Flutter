874d06ed0f0c7181e409c3d21ed0965b4a3b2f66
Adrián Arcalá <aarcala10@gmail.com>
2020-10-12 12:55:33 +0200

    Tests de notebook verdes

---
+++ lib/domain/notebook.dart
@@ -0,0 +1,22 @@
+import 'package:everpobre/domain/note.dart';
+
+class Notebook {
+  static final shared = Notebook();
+  final List<Note> _notes = [];
+
+  int get length => _notes.length;
+
+  bool remove(Note note) {
+    return _notes.remove(note);
+  }
+
+  void add(Note note) {
+    _notes.insert(0, note);
+  }
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return "<runtimeType: $length notes>";
+  }
+}
--- test/notebook_test.dart
+++ test/notebook_test.dart
@@ -1,9 +1,27 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:everpobre/domain/notebook.dart';
+import 'package:everpobre/domain/note.dart';
 
 void main() {
   group("contruction", () {
     test("Can access the shared notebook singleton", () {
       expect(Notebook.shared, isNotNull);
+    });
+  });
+
+  group("contents", () {
+    test("length behaves correctly", () {
+      final nb = Notebook();
+      final nb2 = Notebook();
+      final n = Note("Lorem Ipsum");
+
+      expect(nb.length, 0);
+
+      nb2.add(n);
+      expect(nb2.length, 1);
+
+      nb2.remove(n);
+      expect(nb2.length, 0);
     });
   });
 }
